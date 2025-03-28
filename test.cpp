4639c79544796988d11b2c9510e5adb352051e0f
MthanhGFD <mthanhgfd123@gmail.com>
2025-03-28 16:26:02 +0700

    Code

--- test.cpp
+++ test.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-//
+struct SINHVIEN{
+    char MaSV[10];
+    bool GioiTinh; //True: Nam, False: nữ
+    char NamSinh[4];
+    string HoTenSV;
+    int TrungBinhHK;
+};
